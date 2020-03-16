42c1c65a45e885e297ca8a02b25ba91123e2ce88
Cy Hayakawa <cy.hayakawa@gmail.com>
2020-03-16 19:32:52 -0300

    Starting to implement Board and Piece

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -6,6 +7,9 @@ public class Program {
 
 		Position pos = new Position(3,5);
 		System.out.println(pos);
+		
+		Board board = new Board(8, 8);
+		
 	}
 
 }
